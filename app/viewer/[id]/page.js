568d5ef1bf6a22b08297602f7cbd1cbf9fb0155d
Marvin Pees <deine@mailadresse.tld>
2026-06-13 13:26:25 +0200

    link import fix

--- app/viewer/[id]/page.js
+++ app/viewer/[id]/page.js
@@ -4,6 +4,7 @@
 'use client'
 import { useState, useEffect, useRef } from 'react'
 import { useRouter, useParams } from 'next/navigation'
+import Link from 'next/link'
 import { createClient } from '@/lib/supabase'
 import Navbar from '@/components/Navbar'
 
